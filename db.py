a49ca9cdca9d7b2b82f40683a4f87121863ae3b1
MAIN\SK040122 <tomas.kosegi@anritsu.com>
2018-07-27 16:24:53 +0200

    auth blog

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
@@ -9,12 +10,31 @@ def get_db():
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
-        g.db.row_factory = sqlite3.ROW
+        g.db.row_factory = sqlite3.Row
 
-        return g.db
+        # print("db.py get_db", g.db)
+
+    return g.db
 
 def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
         db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
